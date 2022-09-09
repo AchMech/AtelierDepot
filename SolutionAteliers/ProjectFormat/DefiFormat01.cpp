0e6a05172f85d6291b1e2995fa4019ff4f595cb6
AchMech <2156548@cstj.qc.ca>
2022-09-09 12:28:50 -0400

    debut

---
+++ SolutionAteliers/ProjectFormat/DefiFormat01.cpp
@@ -0,0 +1,22 @@
+//program qui demande un reel et qui garde un certain nombre apres la virgule
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "");
+
+
+	//declaration des vcarible
+	double X;
+
+	cout << fixed;
+	cout << setprecision(2);
+
+	cout << "entre un nombre: ";
+	cin >> X;
+
+	
+}
